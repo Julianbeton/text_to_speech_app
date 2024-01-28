a6f049ea0d9b823033c0c782640d2aa3356b5b17
Julianbeton <julianibeton@iskolarngbayan.pup.edu.ph>
2024-01-29 00:33:01 +0800

    uploading logo

--- text_to_speech_app.py
+++ text_to_speech_app.py
@@ -9,18 +9,20 @@ root = Tk()
 root.title("TEXT TO SPEECH")
 root.geometry("900x450")
 root.resizable(False,False)
-root.configure(bg="#305065")
+root.configure(bg = "#305065")
 
 #icon
-image_icon = PhotoImage(file="speak.png")
+image_icon = PhotoImage(file = "speak.png")
 root.iconphoto(False, image_icon)
 
 #Top Frame
 Top_frame = Frame(root,bg = "white", width = 900, height = 100)
 Top_frame.place(x = 0, y = 0)
 
+Logo = PhotoImage(file = "microphone.png")
+Label(Top_frame, image = Logo, bg = "white").place(x = 0, y = 0)
 
-
+Label(Top_frame, text = "TEXT TO SPEECH", font = "arial 20 bold", bg = "white", fg = "orange").place(x = 100, y = 30)
 
 
 
