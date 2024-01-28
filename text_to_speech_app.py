9e68a63bd108e6621a4bedb1ae79eb440b8513a2
Julianbeton <julianibeton@iskolarngbayan.pup.edu.ph>
2024-01-29 00:21:52 +0800

    uploading top frame

--- text_to_speech_app.py
+++ text_to_speech_app.py
@@ -9,8 +9,25 @@ root = Tk()
 root.title("TEXT TO SPEECH")
 root.geometry("900x450")
 root.resizable(False,False)
+root.configure(bg="#305065")
 
+#icon
 image_icon = PhotoImage(file="speak.png")
 root.iconphoto(False, image_icon)
+
+#Top Frame
+Top_frame = Frame(root,bg = "white", width = 900, height = 100)
+Top_frame.place(x = 0, y = 0)
+
+
+
+
+
+
+
+
+
+
+
 
 root.mainloop()
