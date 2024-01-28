15ca0ae193486fb9e7061076499701512fcee7b3
Julianbeton <julianibeton@iskolarngbayan.pup.edu.ph>
2024-01-29 00:39:45 +0800

    uploading gender combobox

--- text_to_speech_app.py
+++ text_to_speech_app.py
@@ -25,8 +25,13 @@ Label(Top_frame, image = Logo, bg = "white").place(x = 0, y = 0)
 Label(Top_frame, text = "TEXT TO SPEECH", font = "arial 20 bold", bg = "white", fg = "orange").place(x = 100, y = 30)
 
 
+#################
+text_area = Text(root, font = "Robote 20", bg = "white", relief = GROOVE, wrap = WORD)
+text_area.place(x = 10, y = 150, width = 500, height = 250)
 
-
+gender_combobox = Combobox(root, values =["Male", "Female"], font = "arial 14", state = "r", width = 10)
+gender_combobox.place(x = 550, y = 200)
+gender_combobox.set("Male")
 
 
 
